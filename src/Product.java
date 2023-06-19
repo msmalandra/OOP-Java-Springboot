27bdfa0fdac44c7f958826f9d0d50cda7d1deaac
msmalandra <mdos_santos_malandra@itsmeccanicabruzzo.eu>
2023-06-19 12:23:36 +0200

    aggiunta persona e riga in main

---
+++ src/Product.java
@@ -0,0 +1,11 @@
+public class Product {
+    private String product;
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+}
